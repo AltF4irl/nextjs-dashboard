d9e4ad28a3e7f10cd323a01579bb19ba29b8a62f
AltF4irl <altf4irl@gmail.com>
2024-06-18 23:41:22 +0300

    Added Metadata

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -10,7 +10,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <ArrowPathRoundedSquareIcon className="h-12 w-12" />
-      <p className="text-[44px]">gitgud</p>
+      <p className="text-[44px]">Acme</p>
     </div>
   );
 }
